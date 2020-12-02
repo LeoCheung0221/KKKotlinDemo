92b06dbc7d57dd543493f9298d3f862caff1989a
LeoCheung0221 <leocheung4ever@gmail.com>
2020-12-02 09:32:15 +0800

    add basic-grammar module

---
+++ app/src/main/java/com/tufusi/kkkotlin/basic/constructors/WithParams.kt
@@ -0,0 +1,18 @@
+package com.tufusi.kkkotlin.basic.constructors
+
+/**
+ * Created by LeoCheung on 2020/12/1.
+ * @description
+ */
+class WithParams {
+
+    class Man(var name: String)
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val man = Man("LeoCheung")
+            println(man.name)
+        }
+    }
+}
